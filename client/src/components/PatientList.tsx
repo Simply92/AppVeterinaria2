0bffa2926623b22f9449161ceea4e429cdc9e447
Simply92 <pedrouner92@gmail.com>
2024-04-15 20:45:36 -0300

    zustand

--- client/src/components/PatientList.tsx
+++ client/src/components/PatientList.tsx
@@ -1,8 +1,27 @@
+import { usePatientStore } from "../store/store"
+import PatientDetail from "./PatientDetail"
 
 const PatientList = () => {
+    const { patients } = usePatientStore()
     return (
-        <div>
+        <div className="md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll">
+            {patients.length ? (
+                <>
+                    <h2 className="font-black text-3xl text-center">Listado de pacientes</h2>
+                    <p className="text-xl mt-5 mb-10 text-center font-bold">Administra tus pacientes y citas</p>
+                    {patients.map(patient => (
+                        <PatientDetail
+                            key={patient.id}
+                            patient={patient} />
+                    ))}
+                </>
 
+            ) : (
+                <>
+                    <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+                    <p className="text-xl mt-5 mb-10 text-center font-bold">Comienza agregando pacientes</p>
+                </>
+            )}
         </div>
     )
 }
