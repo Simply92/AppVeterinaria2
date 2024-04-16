a86b395831f1a96abe2e369697f0fffc1358acc8
Simply92 <pedrouner92@gmail.com>
2024-04-15 23:34:30 -0300

    final

--- client/src/components/PatientDetail.tsx
+++ client/src/components/PatientDetail.tsx
@@ -1,6 +1,7 @@
 import { Patient } from "../types"
 import PatientDetailItem from "./PatientDetailItem"
 import { usePatientStore } from "../store/store"
+import { toast } from "react-toastify"
 
 type PatientDetailProps = {
     patient: Patient
@@ -8,7 +9,12 @@ type PatientDetailProps = {
 
 const PatientDetail = ({ patient }: PatientDetailProps) => {
 
-    const { deletePatient } = usePatientStore()
+    const { deletePatient, getPatientById } = usePatientStore()
+
+    const handleClick = () => {
+        deletePatient(patient.id)
+        toast.error('Paciente eliminado')
+    }
 
     return (
         <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
@@ -17,16 +23,17 @@ const PatientDetail = ({ patient }: PatientDetailProps) => {
             <PatientDetailItem label="Email" data={patient.email} />
             <PatientDetailItem label="Fecha de alta" data={patient.date.toString()} />
             <PatientDetailItem label="Sintomas" data={patient.symptoms} />
-            <div className="flex justify-between mt-10">
+            <div className="flex flex-col lg:flex-row gap-3 justify-between mt-10">
                 <button
                     type="button"
-                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg">
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                    onClick={() => getPatientById(patient.id)}>
                     Editar
                 </button>
                 <button
                     type="button"
                     className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
-                    onClick={() => deletePatient(patient.id)}>
+                    onClick={handleClick}>
                     Eliminar
                 </button>
             </div>
--- client/src/components/PatientForm.tsx
+++ client/src/components/PatientForm.tsx
@@ -2,19 +2,38 @@ import { useForm } from 'react-hook-form'
 import Error from './Error';
 import type { DraftPatient } from '../types';
 import { usePatientStore } from '../store/store';
+import { useEffect } from 'react';
+import { toast } from "react-toastify"
 
 export default function PatientForm() {
-    const { addPatient } = usePatientStore()
+    const { addPatient, activeId, patients, updatePatient } = usePatientStore()
 
-    const { register, handleSubmit, formState: { errors }, reset } = useForm<DraftPatient>()
+    const { register, handleSubmit, setValue, formState: { errors }, reset } = useForm<DraftPatient>()
+
+    useEffect(() => {
+        if (activeId) {
+            const activePatient = patients.filter(patient => patient.id === activeId)[0]
+            setValue('name', activePatient.name)
+            setValue('caretaker', activePatient.caretaker)
+            setValue('date', activePatient.date)
+            setValue('email', activePatient.email)
+            setValue('symptoms', activePatient.symptoms)
+        }
+    }, [activeId])
 
     const registerPatient = (data: DraftPatient) => {
-        addPatient(data)
+        if (activeId) {
+            updatePatient(data)
+            toast.success('Paciente actualizado correctamente')
+        } else {
+            addPatient(data)
+            toast.success('Paciente registrado correctamente')
+        }
         reset()
     }
 
     return (
-        <div className="md:w-1/2 lg:w-2/5 mx-5">
+        <div className="md:w-1/2 lg:w-2/5 mx-10">
             <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
 
             <p className="text-lg mt-5 text-center mb-10">
